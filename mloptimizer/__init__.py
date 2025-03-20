869a5f5f4d169d35d5f75b3878c4fe2543301329
TurboRx <pranaydebnath645@gmail.com>
2025-03-20 11:11:49 +0530

    Create __init__.py

---
+++ mloptimizer/__init__.py
@@ -0,0 +1,9 @@
+# mloptimizer/__init__.py
+
+"""
+ML-Optimizer: An automated machine learning tool.
+"""
+
+__version__ = "0.1.0"  # Or your desired version number
+
+# You can add more initialization code here if needed.
